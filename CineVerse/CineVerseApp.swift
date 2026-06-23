b6da64caa1c9c7f210db45afa99a5204741e794f
Prashanth Thota <prashanththota@Prashanths-MacBook-Pro.local>
2026-06-23 17:10:38 +0530

    Initial commit

---
+++ CineVerse/CineVerseApp.swift
@@ -0,0 +1,17 @@
+//
+//  CineVerseApp.swift
+//  CineVerse
+//
+//  Created by Prashanth Thota on 23/06/26.
+//
+
+import SwiftUI
+
+@main
+struct CineVerseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CineVerse/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CineVerse
+//
+//  Created by Prashanth Thota on 23/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
